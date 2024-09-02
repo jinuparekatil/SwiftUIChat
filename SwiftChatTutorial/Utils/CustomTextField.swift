5e0ec8c23a5b1d6eada9341473c55788117cf172
Jinu Joy <jinuparekatil@gmail.com>
2024-09-02 17:11:04 +0530

    Impliment Sign in and sign up screen

---
+++ SwiftChatTutorial/Utils/CustomTextField.swift
@@ -0,0 +1,18 @@
+//
+//  CustomTextField.swift
+//  SwiftChatTutorial
+//
+//  Created by Jinu on 02/09/2024.
+//
+
+import SwiftUI
+
+struct CustomTextField: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    CustomTextField()
+}
---
+++ SwiftChatTutorial/View/Authentication/RegistrationView.swift
@@ -0,0 +1,18 @@
+//
+//  RegistrationView.swift
+//  SwiftChatTutorial
+//
+//  Created by Jinu on 02/09/2024.
+//
+
+import SwiftUI
+
+struct RegistrationView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    RegistrationView()
+}
