37793911ece4bac78007fecb43230135a8049691
Jinu Joy <jinuparekatil@gmail.com>
2024-09-02 10:49:33 +0530

    Initial Commit

---
+++ SwiftChatTutorial/SwiftChatTutorialApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftChatTutorialApp.swift
+//  SwiftChatTutorial
+//
+//  Created by Jinu on 02/09/2024.
+//
+
+import SwiftUI
+
+@main
+struct SwiftChatTutorialApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
