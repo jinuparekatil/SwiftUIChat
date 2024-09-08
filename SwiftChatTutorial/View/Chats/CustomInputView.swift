c6ca76a4da7bd097b99eaf83651ebfb54495bfb7
Jinu Joy <jinuparekatil@gmail.com>
2024-09-08 19:16:34 +0530

    Custom input vierw

---
+++ SwiftChatTutorial/View/Chats/CustomInputView.swift
@@ -0,0 +1,18 @@
+//
+//  CustomInputView.swift
+//  SwiftChatTutorial
+//
+//  Created by Jinu on 08/09/2024.
+//
+
+import SwiftUI
+
+struct CustomInputView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    CustomInputView()
+}
---
+++ SwiftChatTutorial/View/Chats/MessageView.swift
@@ -0,0 +1,18 @@
+//
+//  MessageView.swift
+//  SwiftChatTutorial
+//
+//  Created by Jinu on 08/09/2024.
+//
+
+import SwiftUI
+
+struct MessageView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    MessageView()
+}
